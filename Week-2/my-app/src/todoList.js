5bdf02e7d3a3e373d4b5bf9871520b9cf1d5f153
Hazey87 <jfhazey87@outlook.com>
2022-05-07 23:39:24 -0400

    commit

---
+++ Week-2/my-app/src/todoList.js
@@ -0,0 +1,25 @@
+const TodoList = () => {
+    return (
+        <ul>
+            <h2>List of Todos</h2>
+            <li>
+                <input type='checkbox' name='td-1' />
+                <label for='todo-1'>Go To Gym</label>
+            </li>
+            <li>
+                <input type='checkbox' id='td-2' />
+                <label for='td-2'>Do Laundry</label>
+            </li>
+            <li>
+                <input type='checkbox' id='td-3' />
+                <label for='td-3'>Food Shopping</label>
+            </li>
+            <li>
+                <input type='checkbox' id='td-4' />
+                <label for='td-4'>Do Homework</label>
+            </li>
+        </ul>
+    );
+};
+
+export default TodoList;
