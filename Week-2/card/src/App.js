20d24f91fa5b37bae8d7ff37555abdfd5683439a
Hazey87 <jfhazey87@outlook.com>
2022-05-07 22:40:54 -0400

    commit

--- Week-2/card/src/App.js
+++ Week-2/card/src/App.js
@@ -1,3 +1,4 @@
+//!  Second Way With colors changed
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./card";
@@ -5,28 +6,49 @@ import Card from "./card";
 function App() {
     return (
         <div className='wrapper'>
-            <Card
-                title='Title-1'
-                subTitle='Sub Title-1'
-                description='Hello World-1'
-            />
-            <Card
-                title='Title-2'
-                subTitle='Sub Title-2'
-                description='Hello World-2'
-            />
-            <Card
-                title='Title-3'
-                subTitle='Sub Title-3'
-                description='Hello World-3'
-            />
-            <Card
-                title='Title-4'
-                subTitle='Sub Title-4'
-                description='Hello World-4'
-            />
+            <Card/>
         </div>
     );
 }
 
 export default App;
+
+
+
+
+
+
+
+// //! First Way  (color issue)
+// import logo from "./logo.svg";
+// import "./App.css";
+// import Card from "./card";
+
+// function App() {
+//     return (
+//         <div className='wrapper'>
+//             <Card
+//                 title='Title-1'
+//                 subTitle='Sub Title-1'
+//                 description='Hello World-1'
+//             />
+//             <Card
+//                 title='Title-2'
+//                 subTitle='Sub Title-2'
+//                 description='Hello World-2'
+//             />
+//             <Card
+//                 title='Title-3'
+//                 subTitle='Sub Title-3'
+//                 description='Hello World-3'
+//             />
+//             <Card
+//                 title='Title-4'
+//                 subTitle='Sub Title-4'
+//                 description='Hello World-4'
+//             />
+//         </div>
+//     );
+// }
+
+// export default App;
--- Week-2/card/src/card.js
+++ Week-2/card/src/card.js
@@ -1,16 +1,64 @@
 //!         Cards.JS   PAGE
 
-const Card = (props) => {
+//!  Second Way
+const Card = () => {
     return (
         <div className='card'>
-            <div className='cardBody'>
-                <h2 className='card-title'>{props.title}</h2>
-                <h4 className='card-subTitle'>{props.subTitle}</h4>
-                <hr />
-                <p className='card-description'>{props.description}</p>
+            <div className='cardContainer-1'>
+                <div className='cardBody-1'>
+                    <h2 className='card-title'>Title-1</h2>
+                    <h4 className='card-subTitle'>Sub-Title-1</h4>
+                    <hr />
+                    <p className='card-description'>Hello World-1</p>
+                </div>
+            </div>
+
+            <div className='cardContainer-2'>
+                <div className='cardBody-1'>
+                    <h2 className='card-title'>Title-2</h2>
+                    <h4 className='card-subTitle'>Sub-Title-2</h4>
+                    <hr />
+                    <p className='card-description'>Hello World-2</p>
+                </div>
+            </div>
+
+            <div className='cardContainer-3'>
+                <div className='cardBody-1'>
+                    <h2 className='card-title'>Title-3</h2>
+                    <h4 className='card-subTitle'>Sub-Title-3</h4>
+                    <hr />
+                    <p className='card-description'>Hello World-3</p>
+                </div>
+            </div>
+
+            <div className='cardContainer-4'>
+                <div className='cardBody-1'>
+                    <h2 className='card-title'>Title-4</h2>
+                    <h4 className='card-subTitle'>Sub-Title-4</h4>
+                    <hr />
+                    <p className='card-description'>Hello World-4</p>
+                </div>
             </div>
         </div>
     );
 };
 
 export default Card;
+
+// //! First Way to do it (But  couldn't change color  separately per card )
+
+// const Card = ( props ) =>
+// {
+//     return (
+//         <div className='card'>
+//             <div className='cardBody'>
+//                 <h2 className='card-title'>{props.title}</h2>
+//                 <h4 className='card-subTitle'>{props.subTitle}</h4>
+//                 <hr />
+//                 <p className='card-description'>{props.description}</p>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Card;
