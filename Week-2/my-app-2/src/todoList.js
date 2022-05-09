7565425a56b649c6a737c4a74dc62be68271e913
Hazey87 <jfhazey87@outlook.com>
2022-05-08 23:50:10 -0400

    commit

--- Week-2/my-app-2/src/todoList.js
+++ Week-2/my-app-2/src/todoList.js
@@ -5,18 +5,26 @@ const TodoList = () => {
             <li>
                 <input type='checkbox' name='td-1' />
                 <label for='todo-1'>Go To Gym</label>
+                <button>Delete</button>
+                <button>Edit</button>
             </li>
             <li>
                 <input type='checkbox' id='td-2' />
                 <label for='td-2'>Do Laundry</label>
+                <button>Delete</button>
+                <button>Edit</button>
             </li>
             <li>
                 <input type='checkbox' id='td-3' />
                 <label for='td-3'>Food Shopping</label>
+                <button>Delete</button>
+                <button>Edit</button>
             </li>
             <li>
                 <input type='checkbox' id='td-4' />
                 <label for='td-4'>Do Homework</label>
+                <button>Delete</button>
+                <button>Edit</button>
             </li>
         </ul>
     );
