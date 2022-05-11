719a522582fab45de071a0c124a3f459c2e1dad6
Hazey87 <jfhazey87@outlook.com>
2022-05-10 22:32:35 -0400

    commit

--- Week-2/my-app-2/src/store.js
+++ Week-2/my-app-2/src/store.js
@@ -1,34 +1,46 @@
-// const Todos = () =>
-// {return (
-//   [
-//     { text: "Practice code" },
-//     { text: "Practice HTML" },
-//     { text: "Practice JS" },
-//     { text: "Practice CSS" },
-//   ])
-// };
-
-// export default Todos;
-
 import { useState } from "react";
+//! Trash Icon from FontAwesome  (SVG Icons)
+import { FaTrashAlt } from "react-icons/fa";
 
 const Todos_2 = () => {
     const [items, setItems] = useState([
-        { text: "Practice code" },
-        { text: "Practice HTML" },
-        { text: "Practice JS" },
-        { text: "Practice CSS" },
+        {
+            name: "Practice code",
+            id: 20,
+            description: "practice",
+            checked: false,
+        },
+        { name: "Practice HTML", id: 21, description: "HTML", checked: true },
+        { name: "Practice JS", id: 22, description: "JS", checked: false },
+        { name: "Practice CSS", id: 23, description: "CSS", checked: false },
     ]);
+
+    //!  Checkbox Handler
+    const handleCheck = (id) => {
+        const listItems = items.map((item) =>
+            item.id === id ? { ...item, checked: !item.checked } : item
+        );
+        setItems(listItems);
+        // console.log(`key: ${id}`);
+    };
 
     return (
         <header>
             <ul>
                 {items.map((item) => (
                     <li className='item'>
-                        <input type='checkbox' checked={item.checked} />
-                    <label>{item.text}</label>
-                    <button>Delete</button>
-                    <button>Edit</button>
+                        <input
+                            type='checkbox'
+                            checked={item.checked}
+                            onChange={() => handleCheck(item.id)}
+                        />
+                        <label>
+                            ID:{item.id} Name: {item.name} Description:{" "}
+                            {item.description}
+                        </label>
+
+                        <button>Edit</button>
+                        <FaTrashAlt role='button' tabIndex='0' />
                     </li>
                 ))}
             </ul>
@@ -36,3 +48,13 @@ const Todos_2 = () => {
     );
 };
 export default Todos_2;
+
+// //! First Todos Made
+// const Todos_2 = () =>{
+//     const [items, setItems] = useState( [
+//         { text: "Practice code" },
+//         { text: "Practice HTML" },
+//         { text: "Practice JS" },
+//         { text: "Practice CSS" },
+//     ] );
+// }
--- Week-2/my-app-2/src/todoList.js
+++ Week-2/my-app-2/src/todoList.js
@@ -1,3 +1,5 @@
+import { FaTrashAlt } from "react-icons/fa";
+
 const TodoList = () => {
     return (
         <ul>
@@ -5,26 +7,26 @@ const TodoList = () => {
             <li>
                 <input type='checkbox' name='td-1' />
                 <label for='todo-1'>Go To Gym</label>
-                <button>Delete</button>
                 <button>Edit</button>
+                <FaTrashAlt role='button' tabIndex='0' />
             </li>
             <li>
                 <input type='checkbox' id='td-2' />
                 <label for='td-2'>Do Laundry</label>
-                <button>Delete</button>
                 <button>Edit</button>
+                <FaTrashAlt role='button' tabIndex='0' />
             </li>
             <li>
                 <input type='checkbox' id='td-3' />
                 <label for='td-3'>Food Shopping</label>
-                <button>Delete</button>
                 <button>Edit</button>
+                <FaTrashAlt role='button' tabIndex='0' />
             </li>
             <li>
                 <input type='checkbox' id='td-4' />
                 <label for='td-4'>Do Homework</label>
-                <button>Delete</button>
                 <button>Edit</button>
+                <FaTrashAlt role='button' tabIndex='0' />
             </li>
         </ul>
     );
