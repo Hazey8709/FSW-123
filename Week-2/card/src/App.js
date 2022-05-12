0a6bb4a37a241961a1b1f17f443e6e9a0706d552
Hazey87 <jfhazey87@outlook.com>
2022-05-11 22:13:29 -0400

    commit

--- Week-2/card/src/App.js
+++ Week-2/card/src/App.js
@@ -1,25 +1,38 @@
-//!  Second Way With colors changed
-import logo from "./logo.svg";
+//! First Way  (color issue)
 import "./App.css";
 import Card from "./card";
 
 function App() {
     return (
         <div className='wrapper'>
-            <Card/>
+            <Card
+                title='Title-1'
+                subTitle='Sub Title-1'
+                description='Hello World-1'
+                // bgColor={{ backgroundColor: "blue" }}
+            />
+            <Card
+                title='Title-2'
+                subTitle='Sub Title-2'
+                description='Hello World-2'
+            />
+            <Card
+                title='Title-3'
+                subTitle='Sub Title-3'
+                description='Hello World-3'
+            />
+            <Card
+                title='Title-4'
+                subTitle='Sub Title-4'
+                description='Hello World-4'
+            />
         </div>
     );
 }
 
 export default App;
 
-
-
-
-
-
-
-// //! First Way  (color issue)
+// //!  Second Way With colors changed
 // import logo from "./logo.svg";
 // import "./App.css";
 // import Card from "./card";
@@ -27,26 +40,7 @@ export default App;
 // function App() {
 //     return (
 //         <div className='wrapper'>
-//             <Card
-//                 title='Title-1'
-//                 subTitle='Sub Title-1'
-//                 description='Hello World-1'
-//             />
-//             <Card
-//                 title='Title-2'
-//                 subTitle='Sub Title-2'
-//                 description='Hello World-2'
-//             />
-//             <Card
-//                 title='Title-3'
-//                 subTitle='Sub Title-3'
-//                 description='Hello World-3'
-//             />
-//             <Card
-//                 title='Title-4'
-//                 subTitle='Sub Title-4'
-//                 description='Hello World-4'
-//             />
+//             <Card/>
 //         </div>
 //     );
 // }
