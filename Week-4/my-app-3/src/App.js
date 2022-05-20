0d35849288103039e69844212786b8e825eeb8f1
Hazey87 <jfhazey87@outlook.com>
2022-05-20 18:08:35 -0400

    commit

--- Week-4/my-app-3/src/App.js
+++ Week-4/my-app-3/src/App.js
@@ -48,9 +48,11 @@ function App() {
     // }
 
     const addItem = (item) => {
+        console.log("addItem",typeof  item);
         const id = items.length ? items[items.length - 1].id + 1 : 1;
         const myNewItem = { id, checked: false, item };
         const listItems = { ...items, myNewItem };
+
         setItems(listItems);
 
         localStorage.setItem("todosStorage", JSON.stringify(listItems));
