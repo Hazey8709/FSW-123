628f704a363428a5ec3c260d8aac6dc80c2ad7cd
Hazey87 <jfhazey87@outlook.com>
2022-05-23 17:00:31 -0400

    commit

--- Week-5/giphy/src/App.js
+++ Week-5/giphy/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [gif, setGif] = useState("");
+
+    const initialSearch = "baseball";
+
+    const getGif = (findGif = initialSearch) => {
+        console.log(findGif);
+
+        const API = "iejSmIvbU3kYcuiWqyDWd3XM4XYsk0Uw";
+        const URL = `https://api.giphy.com/v1/gifs/search?q=${findGif}&api_key=${API}`;
+
+        fetch(URL)
+            .then((res) => res.json())
+            .then((data) =>
+                setGif(data.data[0].images.fixed_height_downsampled.url)
+            )
+            .catch((error) => console.log(error));
+    };
+
+    const handleForm = (e) => {
+        e.preventDefault();
+        console.log(e.target.search.value);
+        getGif(e.target.search.value);
+    };
+
+    useEffect(getGif, []);
+
+    return (
+        <div className="div-style">
+            <form className='form-style' onSubmit={handleForm}>
+                <input className='input-box' type='text' name='search' />
+                <button className='btn-style' type='submit'>
+                    {" "}
+                    Find Pic{" "}
+                </button>
+            </form>
+            <img className='pic-style' src={gif} alt='' />
+        </div>
+    );
 }
 
 export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
